fe7748c62b407a1bf2c933a44236ad80e21ed865
Sean Conrad <toblerpwn@gmail.com>
2018-03-01 17:22:45 -0800

    create curves - but with some bugs...

--- ios/RNSketchCanvas/RNSketchCanvas/RNSketchData.m
+++ ios/RNSketchCanvas/RNSketchCanvas/RNSketchData.m
@@ -9,11 +9,8 @@
 #import "RNSketchData.h"
 
 @implementation RNSketchData
-{
-    NSMutableArray* _points;
-}
 
-- (instancetype)initWithId:(int) pathId strokeColor:(UIColor*) strokeColor strokeWidth:(int) strokeWidth
+- (instancetype)initWithId:(NSInteger)pathId strokeColor:(UIColor*)strokeColor strokeWidth:(float)strokeWidth
 {
     self = [super init];
     if (self) {
@@ -21,48 +18,25 @@
         _strokeColor = strokeColor;
         _strokeWidth = strokeWidth;
         _points = [[NSMutableArray alloc] init];
-        _cgPoints = NULL;
     }
     return self;
 }
 
-- (instancetype)initWithId:(int) pathId strokeColor:(UIColor*) strokeColor strokeWidth:(int) strokeWidth points: (NSArray*) points
+- (instancetype)initWithId:(NSInteger)pathId strokeColor:(UIColor*) strokeColor strokeWidth:(float)strokeWidth points: (NSArray *)points
 {
     self = [super init];
     if (self) {
         _pathId = pathId;
         _strokeColor = strokeColor;
         _strokeWidth = strokeWidth;
-        _pointCount = points.count;
-        _cgPoints = malloc(sizeof(CGPoint) * _pointCount);
-        for (int i=0; i<_pointCount; i++) {
-            _cgPoints[i] = [points[i] CGPointValue];
-        }
+        _points = points.mutableCopy;
     }
     return self;
-}
-
--(void)dealloc {
-    if (_cgPoints) {
-        free(_cgPoints);
-    }
 }
 
 - (NSArray*)addPoint:(CGPoint) point {
     [_points addObject: [NSValue valueWithCGPoint: point]];
     return _points;
-}
-
-- (void)end {
-    if (!_cgPoints) {
-        _pointCount = _points.count;
-        _cgPoints = malloc(sizeof(CGPoint) * _pointCount);
-        for (int i=0; i<_pointCount; i++) {
-            _cgPoints[i] = [_points[i] CGPointValue];
-        }
-        [_points removeAllObjects];
-        _points = nil;
-    }
 }
 
 @end
